d400bff857c4ce6ca4d2ac065eeaf6f3553679a4
Fabio Cavalcanti <ofabiocavalcanti@gmail.com>
2026-03-30 22:55:58 -0300

    feat(api): adicionar prisma module global

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { loadAppConfig } from './infrastructure/config/app.config';
+import { PrismaModule } from './infrastructure/modules/prisma.module';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { loadAppConfig } from './infrastructure/config/app.config';
       isGlobal: true,
       load: [loadAppConfig],
     }),
+    PrismaModule,
   ],
 })
 export class AppModule {}
